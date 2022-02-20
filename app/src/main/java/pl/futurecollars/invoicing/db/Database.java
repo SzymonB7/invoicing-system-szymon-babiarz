4af8db22841024221c8ff3e0b58ef5318564df1d
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-02-20 14:08:59 +0100

    methods in InMemoryDatabase implemented

--- app/src/main/java/pl/futurecollars/invoicing/db/Database.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/Database.java
@@ -4,10 +4,10 @@ import java.util.List;
 import pl.futurecollars.invoicing.model.Invoice;
 
 public interface Database {
-  void save (Invoice invoice);
+  Integer save (Invoice invoice);
   Invoice getById (Integer id);
   List<Invoice> getAll();
-  Invoice update(Integer id, Invoice updatedInvoice);
+  void update (Integer id, Invoice updatedInvoice);
   void delete (Integer id);
 
 }
